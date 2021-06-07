5061b21bf87cb2403fef7137747c929fbeaffa61
Natali Bystrova <natali.bystrova.external@igraal.com>
2021-06-07 18:32:43 +0300

    Added history object

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const button = document.querySelector('#button');
-const titlesContainer = document.querySelector('#titlesContainer')
+const linksContainer = document.querySelector('#linksContainer')
+const linksContent = document.querySelector('#linksContent')
 
-button.addEventListener('click' , (e) => {
-    e.preventDefault(); 
-    let response = fetch('https://jsonplaceholder.typicode.com/todos')
+document.addEventListener('DOMContentLoaded', function(){ 
+    let response = fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(result => {
-        result.forEach(element => {
-            const title = document.createElement('p')
-            title.innerText = element.title;
-            titlesContainer.appendChild(title)
+        result.forEach((element, index) => {
+            const link = document.createElement('a')
+            const url = `/${index}`
+            const title = document.createElement('h2')
+            title.textContent = element.body
+            const subtitle = document.createElement('p')
+            subtitle.textContent = element.title
+            link.innerText = element.title;
+            link.href = url
+            linksContainer.appendChild(link)
+            link.addEventListener('click', (e) => {
+                e.preventDefault()
+                history.pushState(null, null, url)
+                console.log(linksContent)
+                linksContent.textContent = element.title + element.body
+
+            })
         });
     })
-    
-})
+  });
