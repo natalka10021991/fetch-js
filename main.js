6f179b95a423910a32a8f46199c1622b1b682172
Natali Bystrova <natali.bystrova.external@igraal.com>
2021-06-08 16:32:59 +0300

    Created functions

--- main.js
+++ main.js
@@ -1,37 +1,82 @@
-const button = document.querySelector('#button');
 const linksContainer = document.querySelector('#linksContainer')
+const baseURL = 'https://jsonplaceholder.typicode.com/posts'
+const button = document.createElement('button')
+button.innerText = 'Назад'
+
+
+function displayTitles(array) {
+    array.forEach(element => {
+        let url = `/${element.id}`
+        const link = document.createElement('a')
+        link.id = 'link'
+        link.href = url
+        link.innerText = element.title;
+        linksContainer.appendChild(link)
+    })
+}
+
+function articleLoading(url, array, container) {
+    let objectId = url.substring(url.lastIndexOf("/")+1);
+    let currentObject = array.find(e => e.id == objectId)
+    const content = `
+        <h1>${currentObject.title}</h1>
+        <p>${currentObject.body}</p>
+        `
+    container.innerHTML = content;
+    container.prepend(button)
+}
 
 document.addEventListener('DOMContentLoaded', function(){ 
-    let response = fetch('https://jsonplaceholder.typicode.com/posts')
+    let response = fetch(baseURL)
     .then(response => response.json())
     .then(result => {
-        result.forEach((element) => {
-            const link = document.createElement('a')
-            link.href = '#'
-            const content = `
-              <h1>${element.title}</h1>
-              <p>${element.body}</p>
-              `
-            link.innerText = element.title;
-            linksContainer.appendChild(link)
+
+        displayTitles(result)
+
+        const link = document.querySelectorAll('#link')
+
+        link.forEach( link => {
+
             link.addEventListener('click', (e) => {
                 e.preventDefault()
-                // history.pushState(null, null, url)
-                linksContainer.innerHTML = content;
-                const button = document.createElement('button')
-                button.innerText = 'Назад'
-                linksContainer.prepend(button)
-                button.addEventListener('click', () => {
-                  linksContainer.innerHTML = '';
-                  result.forEach((element) => {
-                    const link2 = document.createElement('a')
-                    link2.href = '#'
-                    link2.innerText = element.title;
-                    linksContainer.appendChild(link2)
-                  })
-                })
+                let url = link.href
+                articleLoading(url, result, linksContainer)
             })
-        });
+        })
+
+        button.addEventListener('click', () => {
+            linksContainer.innerHTML = '';
+            displayTitles(result)
+        })
+
+            // link.addEventListener('click', (e) => {
+
+            //     e.preventDefault()
+            //     let objectId = url.substring(url.lastIndexOf("/")+1);
+            //     let currentObject = result.find(e => e.id == objectId)
+            //     const content = `
+            //         <h1>${currentObject.title}</h1>
+            //         <p>${currentObject.body}</p>
+            //         `
+
+            //     linksContainer.innerHTML = content;
+            //     const button = document.createElement('button')
+            //     button.innerText = 'Назад'
+            //     linksContainer.prepend(button)
+
+            //     button.addEventListener('click', () => {
+
+            //       linksContainer.innerHTML = '';
+
+            //       result.forEach((element) => {
+            //         const link2 = document.createElement('a')
+            //         link2.href = url
+            //         console.log(link2.href)
+            //         link2.innerText = element.title;
+            //         linksContainer.appendChild(link2)
+            //       })
+            //     })
+            // })
     })
   });
 
