7d236cc1fdbe06e889d6e16d898e1b9a209c5d21
Natalia Bystrova <n.a.bystrova@gmail.com>
2021-06-08 11:13:29 +0300

    Finished task

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 const button = document.querySelector('#button');
 const linksContainer = document.querySelector('#linksContainer')
-const linksContent = document.querySelector('#linksContent')
 
 document.addEventListener('DOMContentLoaded', function(){ 
     let response = fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(result => {
-        result.forEach((element, index) => {
+        result.forEach((element) => {
             const link = document.createElement('a')
-            const url = `/${index}`
+            link.href = '#'
             const content = `
-            <h1>${element.title}</h1>
-            <p>${element.body}</p>
-            `
+              <h1>${element.title}</h1>
+              <p>${element.body}</p>
+              `
             link.innerText = element.title;
-            link.href = url
             linksContainer.appendChild(link)
             link.addEventListener('click', (e) => {
                 e.preventDefault()
-                history.pushState(null, null, url)
-                linksContent.innerHTML = content;
+                // history.pushState(null, null, url)
+                linksContainer.innerHTML = content;
+                const button = document.createElement('button')
+                button.innerText = 'Назад'
+                linksContainer.prepend(button)
+                button.addEventListener('click', () => {
+                  linksContainer.innerHTML = '';
+                  result.forEach((element) => {
+                    const link2 = document.createElement('a')
+                    link2.href = '#'
+                    link2.innerText = element.title;
+                    linksContainer.appendChild(link2)
+                  })
+                })
             })
         });
     })
   });
+
